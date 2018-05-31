3dd33ce1d18d8f86666b72c172d7bdf84a06350c
puiyoke <puiyoke8888@hotmail.com>
2018-05-31 23:25:49 +0800

    added avatar to users table, all devise function works properly

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_05_31_150139) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "username", default: "", null: false
+    t.string "avatar"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
